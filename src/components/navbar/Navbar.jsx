843f52e2b31115e81950b0b0ca8862f3cf1c1390
the-everlasting-creators <theeverlastingcreators@gmail.com>
2022-04-17 16:28:38 +1000

    added assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,8 +1,17 @@
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons/ri";
 import "./navbar.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      <div className="gpt3__navbar-links">
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
